f34ec478ce133f7897f2635f4270773789ef7a69
BossBele <adambeleko@gmail.com>
2022-08-07 17:05:45 +0300

    index page

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://github.com/LarvenLLC/next-mailer-server">NextMailer!</a>
         </h1>
 
         <p className={styles.description}>
@@ -39,31 +39,31 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="https://github.com/LarvenLLC/next-mailer-server#readme" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Find in-depth information about NextMailer features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Issues &rarr;</h2>
+            <p>Having trouble with the service? Post your issues and we&apos;ll gladly help.</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
+            href="https://github.com/LarvenLLC/next-mailer-server/fork"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Develop &rarr;</h2>
+            <p>Help us take the project forward by forking our repository. Feel free to make edits and submit your PRs.</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://github.com/LarvenLLC/next-mailer"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Support &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Support the development of next-mailer, the npm package that made this repository possible. Make awesome projects using it, Star it and, if you can, say "hello".
             </p>
           </a>
         </div>
@@ -71,13 +71,13 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/LarvenLLC/next-mailer"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            NextMailer
           </span>
         </a>
       </footer>
