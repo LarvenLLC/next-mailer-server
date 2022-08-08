b42156747f58e36a05ea944b567ec7318d7ae2f7
BossBele <adambeleko@gmail.com>
2022-08-08 18:48:48 +0300

    new mailer api

--- next.config.js
+++ next.config.js
@@ -2,30 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      path: false,
-      constants: false,
-      process: require.resolve('process/browser'),
-      tls: false,
-      net: false,
-      dns: false,
-      child_process: false,
-      // zlib: require.resolve('browserify-zlib'),
-      // stream: require.resolve('stream-browserify'),
-      // util: require.resolve('util'),
-      // buffer: require.resolve('buffer'),
-      // asset: require.resolve('assert'),
-      // crypto: require.resolve('crypto-browserify'),
-      // os: require.resolve('os-browserify/browser'),
-    };
-    // config.node = {
-    //   net: 'empty'
-    // };
-
-    return config;
-  },
 }
 
 module.exports = nextConfig
